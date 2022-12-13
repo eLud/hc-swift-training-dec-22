efb3c7e16b7b5e8435dc8c62aed7cd07952cb678
Ludovic Ollagnier <ludovic@tectec.fr>
2022-12-13 09:53:37 +0100

    Update playground

--- Playgrounds/App Model.playground/Contents.swift
+++ Playgrounds/App Model.playground/Contents.swift
@@ -10,7 +10,7 @@ struct Vinyl: Equatable {
     let albumName: String
     let artist: String
     let releaseDate: Date
-    let numberInSerie: Int
+    let numberInSerie: Int?
     let titles: [String]
     var scratched: Bool
     let speed: Speed
@@ -27,14 +27,35 @@ class Library {
         vinyls.append(vinyl)
     }
 
-    func remove(_ vinyl: Vinyl) {
+    func remove(_ vinyl: Vinyl) -> Vinyl? {
         if let index = vinyls.firstIndex(of: vinyl) {
-            vinyls.remove(at: index)
+            return vinyls.remove(at: index)
         }
+        return nil
     }
 }
 
 let lib = Library()
-let album1 = Vinyl(albumName: "", artist: "", releaseDate: .now, numberInSerie: 0, titles: [], scratched: true, speed: .rpm33)
+let album1 = Vinyl(albumName: "", artist: "", releaseDate: .now, numberInSerie: nil, titles: [], scratched: true, speed: .rpm33)
 
 lib.add(album1)
+
+let numberInSeries: Bool? = lib.remove(album1)?.numberInSerie?.isMultiple(of: 2)
+
+var anOptionalInt: Int? = nil
+anOptionalInt = 6
+
+// nil coalescing operator (valeur par défaut si nil)
+let result = (anOptionalInt ?? 0) * 2
+
+//let result2 = anOptionalInt! * 2
+
+if let anOptionalInt = anOptionalInt {
+    2 * anOptionalInt
+} else {
+    print("No value")
+}
+
+if let anOptionalInt {
+    anOptionalInt * 2
+}
