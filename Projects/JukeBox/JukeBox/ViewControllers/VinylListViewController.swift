e4605c6c3607d32865555c7a8328d4b60588fd8d
Ludovic Ollagnier <ludovic@tectec.fr>
2022-12-14 12:21:42 +0100

    UITableView

--- Projects/JukeBox/JukeBox/ViewControllers/VinylListViewController.swift
+++ Projects/JukeBox/JukeBox/ViewControllers/VinylListViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class VinylListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    let library = Library()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
 
-        // Do any additional setup after loading the view.
+        library.add(Vinyl(albumName: "Vinyl 1", artist: "Artiste", releaseDate: .now, numberInSerie: nil, titles: [], scratched: false, speed: .rpm33))
     }
     
 
@@ -26,4 +32,50 @@ class VinylListViewController: UIViewController {
     }
     */
 
+}
+
+extension VinylListViewController: UITableViewDataSource {
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        library.vinyls.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "vinylCell", for: indexPath)
+
+        let vinyl = library.vinyls[indexPath.row]
+        let title = vinyl.albumName
+        let artist = vinyl.artist
+
+        // iOS > 14
+        var content = cell.defaultContentConfiguration()
+        content.text = title
+        content.secondaryText = artist
+        cell.contentConfiguration = content
+
+        // iOS < 14
+//        cell.textLabel?.text = title
+//        cell.detailTextLabel?.text = artist
+
+        return cell
+    }
+}
+
+extension VinylListViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "My Vinyls"
+        case 1:
+            return "Borrowed Vinyls"
+        default:
+            return nil
+        }
+    }
 }
